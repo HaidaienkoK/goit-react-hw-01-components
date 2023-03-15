dbb3e8f1214182f2bb90f89117e3f4498f33af34
HaidaienkoK <gaidk1973@gmail.com>
2023-03-15 23:03:01 +0200

    end part 1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,39 @@
+import css from './Profile.module.css';
+import PropTypes from 'prop-types';
+
+export const Profile = ({
+  user: { username, tag, location, avatar, stats },
+}) => {
+  return (
+    <div class={css.profile}>
+      <div class={css.description}>
+        <img src={avatar} alt="User avatar" class={css.avatar}></img>
+        <p class={css.name}>{username}</p>
+        <p class={css.tag}>@{tag}</p>
+        <p class={css.location}>{location}</p>
+      </div>
+      <ul class={css.stats}>
+        <li>
+          <span class={css.label}>Followers</span>
+          <span class={css.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span class={css.label}>Views</span>
+          <span class={css.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span class={css.label}>Likes</span>
+          <span class={css.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
+};
